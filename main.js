4d1b70af38ca7584d7aedaa7efefd394742d42c7
ZvonimirZlo <lovrez2@gmail.com>
2024-07-23 17:25:48 +0200

    some minor changes

--- main.js
+++ main.js
@@ -6,31 +6,31 @@ let endingIndex = 8;
 let authorDataArr = [];
 
 fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
-  .then((res) => res.json())
-  .then((data) => {
-    authorDataArr = data;
-    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));  
-  })
-  .catch((err) => {
-   authorContainer.innerHTML = '<p class="error-msg">There was an error loading the authors</p>';
-  });
+    .then((res) => res.json())
+    .then((data) => {
+        authorDataArr = data;
+        displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+    })
+    .catch((err) => {
+        authorContainer.innerHTML = '<p class="error-msg">There was an error loading the authors</p>';
+    });
 
 const fetchMoreAuthors = () => {
-  startingIndex += 8;
-  endingIndex += 8;
+    startingIndex += 8;
+    endingIndex += 8;
 
-  displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
-  if (authorDataArr.length <= endingIndex) {
-    loadMoreBtn.disabled = true;
+    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+    if (authorDataArr.length <= endingIndex) {
+        loadMoreBtn.disabled = true;
 
-loadMoreBtn.style.cursor = 'not-allowed'
-loadMoreBtn.textContent = 'No more data to load';
-}
+        loadMoreBtn.style.cursor = 'not-allowed'
+        loadMoreBtn.textContent = 'No more data to load';
+    }
 };
 
 const displayAuthors = (authors) => {
-authors.forEach(({ author, image, url, bio }, index) => {
-  authorContainer.innerHTML += `
+    authors.forEach(({ author, image, url, bio }, index) => {
+        authorContainer.innerHTML += `
   <div id="${index}" class="user-card">
     <h2 class="author-name">${author}</h2>
     <img class="user-img" src="${image}" alt="${author} avatar">
@@ -39,7 +39,7 @@ authors.forEach(({ author, image, url, bio }, index) => {
     <a class="author-link" href="${url}" target="_blank">${author} author page</a>
   </div>
 `;
-});
+    });
 };
 
 loadMoreBtn.addEventListener('click', fetchMoreAuthors);
